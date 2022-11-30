76458a9d7743d1ef4f05886755788774a664135a
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2022-11-30 11:50:53 -0800

    Begin table creation

--- pages/api/nfts.ts
+++ pages/api/nfts.ts
@@ -4,13 +4,16 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import prismaClient from '../../prisma/prismaClient';
 
 type Data = {
-  events: NFT[]
+  nfts: NFT[]
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  const { owner } = req.query;
+  console.log(owner);
+
   const nfts = await prismaClient.nFT.findMany({});
-  res.status(200).json({ events: nfts });
+  res.status(200).json({ nfts: nfts });
 }
