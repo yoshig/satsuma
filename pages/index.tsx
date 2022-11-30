e8062d900006c16f1e7388627725d04382765d65
Yoshi Gillaspie <yoshi.gillaspie@gmail.com>
2022-11-30 12:53:28 -0800

    Move to grid

--- pages/index.tsx
+++ pages/index.tsx
@@ -80,28 +80,22 @@ export default function Home() {
             : (
               <div>
                 <h2 className="p-2">{nfts.length} NFTs Found</h2>
-                <table className="border-collapse table-auto w-full">
-                  <thead>
-                    <tr className="text-left">
-                      <th className="p-2">Address</th>
-                      <th className="p-2">Owner</th>
-                      <th className="p-2">Price</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {
-                      nfts.map((nft) => {
-                        return (
-                          <tr key={nft.address}>
-                            <td className="p-2">{nft.address}</td>
-                            <td className="p-2">{nft.owner}</td>
-                            <td className="p-2">{nft.priceEth}</td>
-                          </tr>
-                        )
-                      })
-                    }
-                  </tbody>
-                </table>
+                <div className="grid grid-cols-3 gap-4">
+                  <div className="p-2">Address</div>
+                  <div className="p-2">Owner</div>
+                  <div className="p-2">Price</div>
+                </div>
+                {
+                  nfts.map((nft) => {
+                    return (
+                      <div key="nft.address" className="grid grid-cols-3 gap-4">
+                        <div className="p-2">{nft.address}</div>
+                        <div className="p-2">{nft.owner}</div>
+                        <div className="p-2">{nft.priceEth}</div>
+                      </div>
+                    )
+                  })
+                }
               </div>
             )
         }
